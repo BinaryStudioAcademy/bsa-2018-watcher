38554781c9623cb7a394eff8957b3bac2ac394ca
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-08-02 23:18:52 +0300

    Created DashboardsModule

---
+++ frontend/src/app/dashboards/dashboards.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardsRoutingModule } from './dashboards-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DashboardsRoutingModule
+  ],
+  declarations: []
+})
+export class DashboardsModule { }
