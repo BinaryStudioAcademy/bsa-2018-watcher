8a3199f991cdeab2799909d70f7da2ea6b5d4f15
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-08-02 20:00:30 +0300

    landing page with decorators error

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ButtonModule } from '../../node_modules/primeng/primeng';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,11 @@ import { HttpClientModule } from '@angular/common/http';
     LandingComponent
   ],
   imports: [
+    RouterModule.forRoot([]),
     BrowserModule,
     HttpClientModule,
     CoreModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
