e66fe0218257c4750c770be9cda86ea53e80cab3
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-08-02 20:24:13 +0300

    Updated app-routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [{
+path:"",
+redirectTo:"landing",
+pathMatch:"full"
+},
+{
   path: 'landing',
   component: LandingComponent
 }];
