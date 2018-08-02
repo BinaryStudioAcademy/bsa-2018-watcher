f73ddf589e80427d35b7013dd3c821c708c5cdec
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-08-02 20:35:44 +0300

    Added new authorization route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
 
 const routes: Routes = [{
 path:"",
@@ -10,6 +11,10 @@ pathMatch:"full"
 {
   path: 'landing',
   component: LandingComponent
+},
+{
+  path: 'authorization',
+  component: AuthorizationComponent
 }];
 
 @NgModule({
