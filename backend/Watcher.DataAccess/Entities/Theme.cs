1d60a283dbd92556d94ded68f2e93caa0fa365a1
Jam Estonet <bmosiyuk@gmail.com>
2018-08-03 23:11:45 +0300

    Add required attribute to Theme

--- backend/Watcher.DataAccess/Entities/Theme.cs
+++ backend/Watcher.DataAccess/Entities/Theme.cs
@@ -1,8 +1,11 @@
 ﻿namespace Watcher.DataAccess.Entities
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Theme : Entity<int>
     {
         public override int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string FontFamily { get; set; }
         public string BackgroundColor { get; set; }
