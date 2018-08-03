0273d51c2da815e27fadd08f26c67c93638fe491
Jam Estonet <bmosiyuk@gmail.com>
2018-08-03 20:44:11 +0300

    Add User entity without relations

---
+++ backend/Watcher.DataAccess/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Watcher.DataAccess.Entities
+{
+    using System;
+
+    public class User : Entity<int>
+    {
+        public override int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
