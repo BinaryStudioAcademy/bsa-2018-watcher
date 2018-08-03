1dfd79d1cf58d8f6382ea6e3a209f5659c53d24a
Jam Estonet <bmosiyuk@gmail.com>
2018-08-03 22:20:39 +0300

    Add theme entity

---
+++ backend/Watcher.DataAccess/Entities/Theme.cs
@@ -0,0 +1,10 @@
+﻿namespace Watcher.DataAccess.Entities
+{
+    public class Theme : Entity<int>
+    {
+        public override int Id { get; set; }
+        public string Name { get; set; }
+        public string FontFamily { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+}
