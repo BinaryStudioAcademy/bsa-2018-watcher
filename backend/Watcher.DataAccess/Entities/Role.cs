e6134464d4ed0dd40986ef94f764b69e5303b86f
Jam Estonet <bmosiyuk@gmail.com>
2018-08-03 23:12:22 +0300

    Add required attribute to Role

--- backend/Watcher.DataAccess/Entities/Role.cs
+++ backend/Watcher.DataAccess/Entities/Role.cs
@@ -1,8 +1,11 @@
 ﻿namespace Watcher.DataAccess.Entities
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Role : Entity<int>
     {
         public override int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
