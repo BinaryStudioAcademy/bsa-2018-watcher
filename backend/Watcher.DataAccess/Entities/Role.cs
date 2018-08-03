d24b4ef60618eeec18a02df017b2f502a1cf3ade
Jam Estonet <bmosiyuk@gmail.com>
2018-08-03 22:05:48 +0300

    Add role entity

---
+++ backend/Watcher.DataAccess/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Watcher.DataAccess.Entities
+{
+    public class Role : Entity<int>
+    {
+        public override int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
