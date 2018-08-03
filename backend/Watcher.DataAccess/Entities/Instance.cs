eb21618d8f5bc2c3864000cda56e5306118c090b
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-08-03 21:34:54 +0300

    Added entities Instance and Dashboard

---
+++ backend/Watcher.DataAccess/Entities/Instance.cs
@@ -0,0 +1,26 @@
+﻿namespace Watcher.DataAccess.Entities
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    
+    public class Instance : Entity<int>
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override int Id { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        ICollection<Dashboard> Dashboards { get; set; }
+
+        //[Required]
+        //public int OrganizationId { get; set; }
+
+        //[Required]
+        //public Organization Organization { get; set; }
+    }
+}
