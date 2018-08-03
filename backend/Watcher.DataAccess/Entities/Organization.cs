ca3d44887f4f4b9773764a281e152db2c00c0ade
Jam Estonet <bmosiyuk@gmail.com>
2018-08-03 23:17:39 +0300

    ThemeId fk in Organization is nullable

--- backend/Watcher.DataAccess/Entities/Organization.cs
+++ backend/Watcher.DataAccess/Entities/Organization.cs
@@ -9,7 +9,7 @@
         [Required]
         public string Name { get; set; }
 
-        public int ThemeId { get; set; }
+        public int? ThemeId { get; set; }
         public Theme Theme { get; set; }
 
         public IList<UserOrganization> UserOrganizations { get; set; }
