7ea2e711dc0b45805f552c4bb8d76d8962d2f519
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-08-03 20:40:20 +0300

    Created dashboard-header

--- frontend/src/app/dashboards/dashboards.module.ts
+++ frontend/src/app/dashboards/dashboards.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardsRoutingModule } from './dashboards-routing.module';
+import { DashboardsPageComponent } from './dashboards-page/dashboards-page.component';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardsRoutingModule
   ],
-  declarations: []
+  declarations: [DashboardsPageComponent, DashboardHeaderComponent]
 })
 export class DashboardsModule { }
