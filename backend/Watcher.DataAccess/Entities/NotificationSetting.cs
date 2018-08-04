b3f0f78167dff8b4ec428f65f42d577bb0957309
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 07:59:59 +0300

    Add notificationSetting

---
+++ backend/Watcher.DataAccess/Entities/NotificationSetting.cs
@@ -0,0 +1,12 @@
+﻿namespace Watcher.DataAccess.Entities
+{
+    using Watcher.Common.Enums;
+
+    public class NotificationSetting : Entity<int>
+    {
+        public override int Id { get; set; }
+        public NotificationType Type { get; set; }
+        public bool IsMute { get; set; }
+        public bool IsDisable { get; set; }
+    }
+}
