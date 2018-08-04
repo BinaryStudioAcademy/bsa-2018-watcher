b9e0fdbc096bbed00ca2b2a4d3e83ef38d9e1706
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 07:57:22 +0300

    Add notification type enum

---
+++ backend/Watcher.Common/Enums/NotificationType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watcher.Common.Enums
+{
+    public enum NotificationType
+    {
+        System,
+        Chat
+    }
+}
