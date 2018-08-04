3411b4a5064cac921cd5d2cac167144536bc69d4
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 09:22:09 +0300

    Add relation Feedback to User

--- backend/Watcher.DataAccess/Entities/Feedback.cs
+++ backend/Watcher.DataAccess/Entities/Feedback.cs
@@ -9,5 +9,8 @@ namespace Watcher.DataAccess.Entities
         public override int Id { get; set; }
         public string Text { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
