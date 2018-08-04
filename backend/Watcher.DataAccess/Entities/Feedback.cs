6b9cedc0d7559678499d8c8512193a23128f6aaf
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 10:15:58 +0300

    Add required attribute to Feedback

--- backend/Watcher.DataAccess/Entities/Feedback.cs
+++ backend/Watcher.DataAccess/Entities/Feedback.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Watcher.DataAccess.Entities
@@ -7,7 +8,11 @@ namespace Watcher.DataAccess.Entities
     public class Feedback : Entity<int>
     {
         public override int Id { get; set; }
+
+        [Required]
         public string Text { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
 
         public int UserId { get; set; }
