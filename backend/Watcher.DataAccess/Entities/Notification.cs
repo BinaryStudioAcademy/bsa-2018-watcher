8e5309adefaea12721ef2f5576b690510dbcd999
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 07:45:34 +0300

    Add notification without relations

---
+++ backend/Watcher.DataAccess/Entities/Notification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watcher.DataAccess.Entities
+{
+    public class Notification : Entity<int>
+    {
+        public override int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
