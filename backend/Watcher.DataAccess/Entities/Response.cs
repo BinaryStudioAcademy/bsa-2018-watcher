1e661ae5fd83b99799ad9fdacf02f0f6edec740b
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 10:23:15 +0300

    Add required attribute to Response

--- backend/Watcher.DataAccess/Entities/Response.cs
+++ backend/Watcher.DataAccess/Entities/Response.cs
@@ -1,11 +1,16 @@
 ﻿namespace Watcher.DataAccess.Entities
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class Response : Entity<int>
     {
         public override int Id { get; set; }
+
+        [Required]
         public string Text { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
 
         public int UserId { get; set; }
