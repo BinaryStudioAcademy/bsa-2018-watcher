b60d51fb82c277501f39a07df56f2ab42e1fdfb1
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 10:23:58 +0300

    Change style of Theme.cs

--- backend/Watcher.DataAccess/Entities/Theme.cs
+++ backend/Watcher.DataAccess/Entities/Theme.cs
@@ -5,9 +5,12 @@
     public class Theme : Entity<int>
     {
         public override int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         public string FontFamily { get; set; }
+
         public string BackgroundColor { get; set; }
     }
 }
