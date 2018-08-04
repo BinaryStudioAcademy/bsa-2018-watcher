168df0eb7f0e14970b16fd6ff17bde1181faddbc
Jam Estonet <bmosiyuk@gmail.com>
2018-08-04 09:37:15 +0300

    Add Response

---
+++ backend/Watcher.DataAccess/Entities/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watcher.DataAccess.Entities
+{
+    public class Response : Entity<int>
+    {
+        public override int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        
+    }
+}
