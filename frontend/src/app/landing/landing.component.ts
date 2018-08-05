19d29ec41c2f74cf59ff9564d5249299a8840db4
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-08-05 17:09:18 +0300

    fixed bug with hamburger

--- frontend/src/app/landing/landing.component.ts
+++ frontend/src/app/landing/landing.component.ts
@@ -17,19 +17,8 @@ export class LandingComponent implements OnInit, OnDestroy {
     const scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if (scrolled < 100) {
       document.getElementById('header').style.background = 'rgba(0,0,0,0.2)';
-    } else { document.getElementById('header').style.background = 'rgba(0,0,0,0.8)'; }
+    } else { document.getElementById('header').style.background = 'rgba(0,0,0,0.8)'; }}
 
-    const dw_getScrollOffsets = () => {
-      const doc = document, w = window;
-      let y, docEl;
-            if (typeof w.pageYOffset === 'number') {
-              y = w.pageYOffset; } else {
-                  docEl = (doc.compatMode && doc.compatMode === 'CSS1Compat') ?
-                  doc.documentElement : doc.body;
-                  y = docEl.scrollTop;
-                }
-            return y;
-  }; }
   scrollTo(id: string) {
     const element = document.getElementById(id);
     element.scrollIntoView( {block: 'start', behavior: 'smooth'});
@@ -40,14 +29,20 @@ export class LandingComponent implements OnInit, OnDestroy {
   }
   show() {
     const menu = document.getElementById('nav');
-    if (menu.style.display === 'block') {
-      menu.style.display = 'none'; } else { menu.style.display = 'block'; }
+    if (menu.style.visibility === 'visible') {
+      menu.style.visibility = 'hidden'; } else { menu.style.visibility = 'visible'; }
   }
 
+  resize() {
+    if (window.innerWidth > 730) {document.getElementById('nav').style.visibility = 'visible'; }
+  }
   ngOnInit() {
+    window.addEventListener('resize', this.resize, true);
     window.addEventListener('scroll', this.headerScroll, true);
   }
   ngOnDestroy() {
     window.removeEventListener('scroll', this.headerScroll, true);
+    window.removeEventListener('resize', this.resize, true);
+
 }
 }
