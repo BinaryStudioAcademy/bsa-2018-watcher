7f8573102c7af0ac0ee042e5f793f8d872be00cf
Anastasiia <nastena31011993@gmail.com>
2018-08-07 15:08:29 +0300

    Added UserModel

--- frontend/src/app/authorization/authorization.component.ts
+++ frontend/src/app/authorization/authorization.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef, ViewChild  } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { AuthService } from '../core/services/auth.service';
@@ -21,13 +21,13 @@ export class AuthorizationComponent implements OnInit {
   isSignIn = true;
   isSuccessSignUp = false;
 
-  companyName =  '';
+  companyName = '';
   lastName = '';
   firstName = '';
 
   constructor(private authService: AuthService, private router: Router) {
 
-   }
+  }
 
   ngOnInit() {
   }
@@ -88,10 +88,38 @@ export class AuthorizationComponent implements OnInit {
   }
 
   signInWithGoogle() {
-    this.authService.signInWithGoogle()
-    .then((res) => {
-      debugger;
-        var token : FirebaseCredential = res.credential;
+    console.log(this.authService.signInWithGoogle()
+    .then((res) => {console.log(res);}));
+  }
+
+  signInWithFacebook() {
+    this.authService.signInWithFacebook()
+      .then((res) => {
+
+        // var token : FirebaseCredential = res.credential;
+        // var user : firebase.User = res.user;
+
+        this.router.navigate(['landing']);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  signInWithTwitter() {
+    this.authService.signInWithTwitter()
+      .then((res) => {
+        // var token : FirebaseCredential = res.credential;
+        // var user : firebase.User = res.user;
+
+        this.router.navigate(['landing']);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  signInWithGitHub() {
+    this.authService.signInWithGitHub()
+      .then((res) => {
+        debugger;
+        var token: FirebaseCredential = res.credential;
         var user = res.user.getIdToken(true).then(idToken => {
           var i = idToken;
           console.log(i);
@@ -101,41 +129,7 @@ export class AuthorizationComponent implements OnInit {
         debugger;
         this.router.navigate(['landing']);
       })
-    .catch((err) => console.log(err));
-  }
-
-  signInWithFacebook() {
-    this.authService.signInWithFacebook()
-    .then((res) => {
-
-        // var token : FirebaseCredential = res.credential;
-        // var user : firebase.User = res.user;
-
-        this.router.navigate(['landing']);
-      })
-    .catch((err) => console.log(err));
-  }
-
-  signInWithTwitter() {
-    this.authService.signInWithTwitter()
-    .then((res) => {
-        // var token : FirebaseCredential = res.credential;
-        // var user : firebase.User = res.user;
-
-        this.router.navigate(['landing']);
-      })
-    .catch((err) => console.log(err));
-  }
-
-  signInWithGitHub() {
-    this.authService.signInWithGitHub()
-    .then((res) => {
-        // var token : FirebaseCredential = res.credential;
-        // var user : firebase.User = res.user;
-
-        this.router.navigate(['landing']);
-      })
-    .catch((err) => console.log(err));
+      .catch((err) => console.log(err));
   }
 
 
