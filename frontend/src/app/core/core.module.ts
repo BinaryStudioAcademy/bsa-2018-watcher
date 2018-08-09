cd7028f3a8187aa16572d337a8e19cd50225de8b
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-08-09 08:15:01 +0300

    fixed errors

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { throwIfAlreadyLoaded } from './guards/module-import.guard';
 import { CommonModule } from '@angular/common';
 
-import { DefaultService } from './services/default.service';
+// import { DefaultService } from './services/default.service';
 import { ToastrService } from './services/toastr.service';
 import { MessageService, ConfirmationService } from 'primeng/api';
 
@@ -10,7 +10,7 @@ import { MessageService, ConfirmationService } from 'primeng/api';
   imports: [
     CommonModule
   ],
-  providers: [ DefaultService, ToastrService, ConfirmationService, MessageService ],
+  providers: [ ToastrService, ConfirmationService, MessageService ],
   declarations: []
 })
 export class CoreModule {
