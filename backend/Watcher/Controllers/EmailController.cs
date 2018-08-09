28c3073237c29c7c64b9225de3093fab2704b85e
tetiana-zaichuk <tanyazaychuk@gmail.com>
2018-08-09 23:36:13 +0300

    Added EmailController for testing

---
+++ backend/Watcher/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Watcher.Core.Interfaces;
+using Watcher.Core.Providers;
+
+namespace Watcher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private IEmailProvider _emailProvider;
+
+        public EmailController(IEmailProvider emailProvider)
+        {
+            _emailProvider = emailProvider;
+        }
+
+        public async Task SendEmail()
+        {
+            await _emailProvider.SendMessage("tt@gmail.com", "Try", new List<string>(){ "tanyazaychuk@gmail.com" }, "msg", "msgHtml");
+        }
+    }
+}
--- backend/Watcher/Startup.cs
+++ backend/Watcher/Startup.cs
@@ -126,6 +126,7 @@
                     })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddJsonOptions(MvcSetup.JsonSetupAction);
+            services.AddScoped<IEmailProvider, EmailProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
