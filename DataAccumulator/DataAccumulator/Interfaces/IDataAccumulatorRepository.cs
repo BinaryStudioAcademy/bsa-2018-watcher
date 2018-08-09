45ae397c86c0fc3458844d3ca44a526f3f0154bb
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-10 01:17:57 +0300

    Added DataAccumulatorRepository

---
+++ DataAccumulator/DataAccumulator/Interfaces/IDataAccumulatorRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataAccumulator.Entities;
+using DataAccumulator.Models;
+
+namespace DataAccumulator.Interfaces
+{
+    public interface IDataAccumulatorRepository
+    {
+        Task<IEnumerable<CollectedData>> GetAllEntities();
+
+        Task<CollectedData> GetEntity(string id);
+
+        // query after multiple parameters
+        Task<IEnumerable<CollectedData>> GetEntity(DateTime time);
+
+        // add new entity
+        Task AddEntity(CollectedData item);
+
+        // update single entity
+        Task<bool> UpdateEntity(string id, CollectedDataDto collectedData);
+
+        // remove a single entity
+        Task<bool> RemoveEntity(string id);
+
+        Task<bool> RemoveAllEntities();
+
+        // creates index
+        Task<string> CreateIndex();
+    }
+}
---
+++ DataAccumulator/DataAccumulator/Repositories/DataAccumulatorRepository.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using DataAccumulator.Data;
+using DataAccumulator.Entities;
+using DataAccumulator.Interfaces;
+using DataAccumulator.Models;
+
+namespace DataAccumulator.Repositories
+{
+    public class DataAccumulatorRepository : IDataAccumulatorRepository
+    {
+        private readonly DataAccumulatorContext _context = null;
+
+        public DataAccumulatorRepository(IOptions<Settings> settings)
+        {
+            _context = new DataAccumulatorContext(settings);
+        }
+
+        public async Task<IEnumerable<CollectedData>> GetAllEntities()
+        {
+            try
+            {
+                return await _context.Datasets.Find(_ => true).ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<CollectedData> GetEntity(string id)
+        {
+            try
+            {
+                ObjectId internalId = GetInternalId(id);
+
+                return await _context.Datasets
+                    .Find(data => data.Id == id || data.InternalId == internalId)
+                    .FirstOrDefaultAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        // Query by time
+        public async Task<IEnumerable<CollectedData>> GetEntity(DateTime time)
+        {
+            try
+            {
+                var query = _context.Datasets
+                    .Find(data => data.Time >= time);
+
+                return await query.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task AddEntity(CollectedData item)
+        {
+            try
+            {
+                await _context.Datasets.InsertOneAsync(item);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<bool> UpdateEntity(string id, CollectedDataDto collectedDataDto)
+        {
+            try
+            {
+                // todo
+                var collectedData = new CollectedData()
+                {
+                    Id = collectedDataDto.Id
+                };
+
+                ReplaceOneResult actionResult = await _context.Datasets
+                    .ReplaceOneAsync(data => data.Id.Equals(id), collectedData, new UpdateOptions { IsUpsert = true });
+
+                return actionResult.IsAcknowledged && actionResult.ModifiedCount > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<bool> RemoveEntity(string id)
+        {
+            try
+            {
+                DeleteResult actionResult =
+                    await _context.Datasets
+                        .DeleteOneAsync(Builders<CollectedData>.Filter.Eq("Id", id));
+
+                return actionResult.IsAcknowledged && actionResult.DeletedCount > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<bool> RemoveAllEntities()
+        {
+            try
+            {
+                DeleteResult actionResult = await _context.Datasets
+                    .DeleteManyAsync(new BsonDocument());
+
+                return actionResult.IsAcknowledged && actionResult.DeletedCount > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        // It creates a sample compound index 
+        // MongoDb automatically detects if the index already exists - in this case it just returns the index details
+        public async Task<string> CreateIndex()
+        {
+            try
+            {
+                IndexKeysDefinition<CollectedData> keys = Builders<CollectedData>
+                    .IndexKeys
+                    .Ascending(item => item.ProcessesCount)
+                    .Ascending(item => item.Time);
+
+                return await _context.Datasets
+                    .Indexes
+                    .CreateOneAsync(new CreateIndexModel<CollectedData>(keys));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        // Try to convert the Id to a BSonId value
+        private ObjectId GetInternalId(string id)
+        {
+            if (!ObjectId.TryParse(id, out ObjectId internalId))
+                internalId = ObjectId.Empty;
+
+            return internalId;
+        }
+    }
+}
