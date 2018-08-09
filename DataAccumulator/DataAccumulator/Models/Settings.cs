3976d7dd146dfc60e534075f42135fc80ca56c39
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-10 01:11:31 +0300

    Added Settings class

---
+++ DataAccumulator/DataAccumulator/Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccumulator.Models
+{
+    public class Settings
+    {
+        public string ConnectionString;
+        public string Database;
+    }
+}
