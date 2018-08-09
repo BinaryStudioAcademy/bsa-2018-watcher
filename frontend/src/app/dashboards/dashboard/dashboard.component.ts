eebc01147d99d77955773527979a86f3d2af4e74
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-08-09 12:01:02 +0300

    tabs

--- frontend/src/app/dashboards/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboards/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, OnChanges } from '@angular/core';
 import { MenuItem, MenuItemContent } from '../../../../node_modules/primeng/primeng';
 import { CreateEditDashboardComponent } from '../create-edit-dashboard/create-edit-dashboard.component';
-import {Dashboard} from '../../shared/models/dashboard.model';
+import { DashboardService } from '../../core/services/dashboard.service';
+import { Dashboard } from '../../shared/models/dashboard';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,31 +14,42 @@ export class DashboardComponent implements OnInit {
 
 @ViewChild(CreateEditDashboardComponent) popup: CreateEditDashboardComponent;
 
-items: MenuItem[];
+inctanceId: number;
+menuItems: MenuItem[];
+dashboards: Dashboard[];
 activeItem: MenuItem;
 newDashboard: Dashboard;
 
-  constructor() { }
+  constructor(private service: DashboardService) {
+    this.menuItems = [];
+    this.dashboards = [];
+  }
 
   createDashboard() {
    this.newDashboard.title = this.popup.dashboardTitle;
    this.newDashboard.createdAt = new Date();
   }
 
+  getDashboards() {
+    /*this.service.getAllByInstance(this.inctanceId).subscribe((data: Dashboard[]) => {
+      this.dashboards = data;
+     });*/
+      this.dashboards = [
+       new Dashboard('RAM',  new Date(),  this.inctanceId),
+       new Dashboard('CAM',  new Date(),  this.inctanceId),
+       new Dashboard('DAM',  new Date(),  this.inctanceId), ];
+      this.dashboards.forEach(dash => {this.menuItems.push({label: dash.title});
+      } );
+  }
   ngOnInit() {
-    this.items = [
-      {label: 'RAM', },
-      {label: 'CAM', },
-      {label: 'DAM', },
-      {label: 'Add new', command: (onlick) => {
-        this.popup.display = true;
-        const index: number = this.items.length - 2;
-        console.log(index);
-        this.activeItem = this.items[index];
-    }}
-  ];
-  this.activeItem = this.items[0];
-  this.popup.ngOnInit();
+    this.getDashboards();
+    this.menuItems.push(  {label: 'Add new', command: (onlick) => {
+      this.popup.display = true;
+      const index: number = this.menuItems.length - 2;
+      console.log(index);
+      this.activeItem = this.menuItems[index];
+  }} );
+  this.activeItem = this.menuItems[0];
   }
 
 }
--- frontend/src/app/shared/models/dashboard.ts
+++ frontend/src/app/shared/models/dashboard.ts
@@ -1,7 +1,13 @@
 import { Instance } from './instance.model';
 import { Chart } from './chart.model';
 
-export interface Dashboard {
+export class Dashboard {
+
+  constructor(title, createdAt, instanceId) {
+    this.title = title;
+    this.createdAt = createdAt;
+    this.instanceId = instanceId;
+  }
   id: number;
   title: string;
   createdAt: Date;
