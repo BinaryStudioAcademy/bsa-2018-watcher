f2c3fed63fd36e68e5f417fc20a2edcca7ccdece
Jam Estonet <bmosiyuk@gmail.com>
2018-08-10 20:40:59 +0300

    Add shell module and shell component

---
+++ frontend/src/app/shell/shell.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell/shell.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ShellComponent]
+})
+export class ShellModule { }
