c4e134420429c7d4943539c40684b893114c7ee1
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-12 23:46:27 +0300

    Added custom exceptions

---
+++ DataAccumulator/DataAccumulator/Exceptions/BadRequestException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccumulator.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+        {
+        }
+
+        public BadRequestException(string message)
+            : base(message)
+        {
+        }
+
+        public BadRequestException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ DataAccumulator/DataAccumulator/Exceptions/NotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccumulator.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
