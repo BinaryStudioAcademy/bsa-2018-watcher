28bb89e6915e55f2467f26d5eec1c358a868d399
kostyaHrytsyuk <streetman889@gmail.com>
2018-08-13 16:57:15 +0300

    Added handling 404 error

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LandingComponent} from './landing/landing.component';
+import { ShellComponent } from './shell/shell.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [{
   path: '',
-  component: LandingComponent
-}];
+  component: LandingComponent,
+  }, {
+  path: '**',
+  redirectTo: 'user/dashboards',
+  canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
