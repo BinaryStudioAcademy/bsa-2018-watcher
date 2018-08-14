1f5419abe82d17c2c55975f9e82fe24262597624
tetiana-zaichuk <tanyazaychuk@gmail.com>
2018-08-14 17:01:36 +0300

    Added two enums

--- frontend/src/app/shell/shell.component.ts
+++ frontend/src/app/shell/shell.component.ts
@@ -5,8 +5,6 @@ import {Component} from '@angular/core';
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.sass']
 })
-  feedback: boolean;
-     this.feedback = true;
 
 export class ShellComponent {
   constructor() {  }
