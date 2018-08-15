1b4df6162eed5212d549daca7eed93634479b322
Pavel <5avel@hotmail.com>
2018-08-15 15:58:37 +0300

    Added Dto to Invite

---
+++ backend/Watcher.Common/Dtos/OrganizationInvitesDto.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Watcher.Common.Dtos
+{
+    using System;
+    using Watcher.Common.Enums;
+    using Watcher.Common.Interfaces.Entities;
+    using Watcher.Common.MoveToFrontend;
+
+    [ExportClassToTypescript]
+    public class OrganizationInvitesDto : IEntity<int>
+    {
+        public int Id { get; set; }
+
+
+        public string Link { get; set; }
+
+        public int OrganizationId { get; set; }
+        public OrganizationDto Organization { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime ExperationDate { get; set; }
+
+        public string CreatedByUserId { get; set; }
+        public UserDto CreatedByUser { get; set; }
+
+        public OrganizationInviteState State { get; set; }
+
+    }
+}
