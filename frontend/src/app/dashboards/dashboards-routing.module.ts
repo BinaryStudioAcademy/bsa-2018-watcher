f5b122dc26857f1f87f8636743c42a0aa4d00997
Rostyslav Diakiv <rostyslav.diakiv@gmail.com>
2018-08-16 16:31:52 +0300

    refactored code

--- frontend/src/app/dashboards/dashboards-routing.module.ts
+++ frontend/src/app/dashboards/dashboards-routing.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditInstanceComponent } from './edit-instance/edit-instance.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {EditInstanceComponent} from './edit-instance/edit-instance.component';
 
 
-const dashboardRoutes: Routes = [{
-  path: 'dashboards',
-  component: DashboardComponent},
+const dashboardRoutes: Routes = [
+  {
+    path: 'dashboards',
+    component: DashboardComponent
+  },
   {
     path: 'edit-instance/:id',
     component: EditInstanceComponent
-},{
+  },
+  {
     path: 'edit-instance',
     component: EditInstanceComponent
-}];
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(dashboardRoutes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule {}
+export class DashboardRoutingModule {
+}
