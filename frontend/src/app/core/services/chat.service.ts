b72b95fbdec9e5a5e40fc396b476be4d043a3a16
Volodymyr <volodymyr_bilyk@outlook.com>
2018-08-16 15:24:45 +0300

    Added extra services

---
+++ frontend/src/app/core/services/chat.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { Chat } from '../../shared/models/chat.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationService {
+  private readonly ctrlUrl = 'chats';
+
+  constructor(private apiService: ApiService) {
+  }
+
+  getAll(): Observable<Chat[]> {
+    return this.apiService.get(`/${this.ctrlUrl}`);
+  }
+
+  get(id: number): Observable<Chat> {
+    return this.apiService.getById(`/${this.ctrlUrl}`, id);
+  }
+
+  create(chat: Chat) {
+    return this.apiService.post(`/${this.ctrlUrl}`, chat);
+  }
+
+  update(id: number, chat: Chat) {
+    return this.apiService.put(`/${this.ctrlUrl}/${id}`, chat);
+  }
+
+  delete(id: number): Observable<Chat> {
+    return this.apiService.delete(`/${this.ctrlUrl}/${id}`);
+  }
+}
