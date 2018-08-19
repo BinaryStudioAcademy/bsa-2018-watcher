d04c3de249a547c9a2ea715612f9527cea769a6f
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2018-08-19 13:24:19 +0300

    fixed guid

--- DataCollector/DataCollector/DataSender.cs
+++ DataCollector/DataCollector/DataSender.cs
@@ -28,6 +28,7 @@ namespace DataCollector
             IFormatter formatter = new BinaryFormatter(); // the formatter that will serialize my object on my stream 
 
             var myContent = JsonConvert.SerializeObject(dataItem);
+            Console.WriteLine(myContent);
             var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
             var byteContent = new ByteArrayContent(buffer);
             byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
