97d78c0a7c87401b313084f12183ff386a47f299
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-23 15:24:48 +0300

    Updated DataAggregatorCore

--- DataAccumulator/DataAccumulator.DataAggregator/DataAggregatorCore.cs
+++ DataAccumulator/DataAccumulator.DataAggregator/DataAggregatorCore.cs
@@ -32,51 +32,54 @@ namespace DataAccumulator.DataAggregator
             var accumulatedCollectedDataDtos = 
                 await _aggregatorService.GetAccumulatorEntitiesAsync(timeFrom, timeTo);
 
-            var collectedDataDtosAverage =
-                from collectedDataDto in accumulatedCollectedDataDtos
-                group collectedDataDto by collectedDataDto.ClientId
-                into collectedDataGroup
-                select new CollectedDataDto
+            if (accumulatedCollectedDataDtos != null)
+            {
+                var collectedDataDtosAverage =
+                    from collectedDataDto in accumulatedCollectedDataDtos
+                    group collectedDataDto by collectedDataDto.ClientId
+                    into collectedDataGroup
+                    select new CollectedDataDto
+                    {
+                        ClientId = collectedDataGroup.Key,
+                        ProcessesCount = Convert.ToInt32(collectedDataGroup
+                            .Average(d => d.ProcessesCount)),
+                        CpuUsagePercent = collectedDataGroup
+                            .Average(d => d.CpuUsagePercent),
+                        RamUsagePercent = collectedDataGroup
+                            .Average(d => d.RamUsagePercent),
+                        InterruptsTimePercent = collectedDataGroup
+                            .Average(d => d.InterruptsTimePercent),
+                        LocalDiskFreeSpacePercent = collectedDataGroup
+                            .Average(d => d.LocalDiskFreeSpacePercent),
+                        AvaliableRamBytes = collectedDataGroup
+                            .Average(d => d.AvaliableRamBytes),
+                        InterruptsPerSeconds = collectedDataGroup
+                            .Average(d => d.InterruptsPerSeconds),
+                        LocalDiskFreeMBytes = collectedDataGroup
+                            .Average(d => d.LocalDiskFreeMBytes),
+                        Time = collectedDataGroup
+                            .Max(d => d.Time),
+                        ProcessesCPU = collectedDataGroup
+                            .SelectMany(d => d.ProcessesCPU)
+                            .ToLookup(pair => pair.Key, pair => pair.Value)
+                            .ToDictionary(l => l.Key, l => l.Average()),
+                        ProcessesRAM = collectedDataGroup
+                            .SelectMany(d => d.ProcessesRAM)
+                            .ToLookup(pair => pair.Key, pair => pair.Value)
+                            .ToDictionary(l => l.Key, l => l.Average()),
+                    };
+
+                // Save aggregated CollectedDataDto to Aggregator table MongoDb
+                foreach (var collectedDataDto in collectedDataDtosAverage)
                 {
-                    ClientId = collectedDataGroup.Key,
-                    ProcessesCount = Convert.ToInt32(collectedDataGroup
-                        .Average(d => d.ProcessesCount)),
-                    CpuUsagePercent = collectedDataGroup
-                        .Average(d => d.CpuUsagePercent),
-                    RamUsagePercent = collectedDataGroup
-                        .Average(d => d.RamUsagePercent),
-                    InterruptsTimePercent = collectedDataGroup
-                        .Average(d => d.InterruptsTimePercent),
-                    LocalDiskFreeSpacePercent = collectedDataGroup
-                        .Average(d => d.LocalDiskFreeSpacePercent),
-                    AvaliableRamBytes = collectedDataGroup
-                        .Average(d => d.AvaliableRamBytes),
-                    InterruptsPerSeconds = collectedDataGroup
-                        .Average(d => d.InterruptsPerSeconds),
-                    LocalDiskFreeMBytes = collectedDataGroup
-                        .Average(d => d.LocalDiskFreeMBytes),
-                    Time = collectedDataGroup
-                        .Max(d => d.Time),
-                    ProcessesCPU = collectedDataGroup
-                        .SelectMany(d => d.ProcessesCPU)
-                        .ToLookup(pair => pair.Key, pair => pair.Value)
-                        .ToDictionary(l => l.Key, l => l.Average()),
-                    ProcessesRAM = collectedDataGroup
-                        .SelectMany(d => d.ProcessesRAM)
-                        .ToLookup(pair => pair.Key, pair => pair.Value)
-                        .ToDictionary(l => l.Key, l => l.Average()),
-                };
+                    await _aggregatorService.AddAggregatorEntityAsync(collectedDataDto);
+                }
 
-            // Save aggregated CollectedDataDto to Aggregator table MongoDb
-            foreach (var collectedDataDto in collectedDataDtosAverage)
-            {
-                await _aggregatorService.AddAggregatorEntityAsync(collectedDataDto);
-            }
-
-            // Delete already aggregated CollectedDataDto from Accumulator table MongoDb
-            foreach (var collectedDataDto in accumulatedCollectedDataDtos)
-            {
-                await _aggregatorService.DeleteAccumulatorEntityAsync(collectedDataDto.Id);
+                // Delete already aggregated CollectedDataDto from Accumulator table MongoDb
+                foreach (var collectedDataDto in accumulatedCollectedDataDtos)
+                {
+                    await _aggregatorService.DeleteAccumulatorEntityAsync(collectedDataDto.Id);
+                }
             }
         }
     }
