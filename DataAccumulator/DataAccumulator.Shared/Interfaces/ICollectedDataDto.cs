cc549b7d89ab707e136ca70932d83dfeb10f5160
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-23 12:29:39 +0300

    Updated IEntity

--- DataAccumulator/DataAccumulator.Shared/Interfaces/ICollectedDataDto.cs
+++ DataAccumulator/DataAccumulator.Shared/Interfaces/ICollectedDataDto.cs
@@ -1,6 +1,10 @@
-﻿namespace DataAccumulator.Shared.Interfaces
+﻿using System;
+
+namespace DataAccumulator.Shared.Interfaces
 {
     public interface ICollectedDataDto
     {
+        Guid Id { get; set; }
+        Guid ClientId { get; set; }
     }
 }
