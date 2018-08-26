85482f02b9d538b1b70276bbba141f2efbff55cb
Anastasiia <nastena31011993@gmail.com>
2018-08-26 14:56:41 +0300

    Added put method to update user partially

--- backend/Watcher.DataAccess/Interfaces/Repositories/IUsersRepository.cs
+++ backend/Watcher.DataAccess/Interfaces/Repositories/IUsersRepository.cs
@@ -1,8 +1,10 @@
 ﻿namespace Watcher.DataAccess.Interfaces.Repositories
 {
+    using System.Threading.Tasks;
     using Watcher.DataAccess.Entities;
 
     public interface IUsersRepository : IRepository<User, string>
     {
+        Task<User> UpdateProfileAsync(User entity);
     }
 }
