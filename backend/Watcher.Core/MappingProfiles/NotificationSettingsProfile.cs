76714734f475b968b15257caeda5c471c6292a89
Jam Estonet <bmosiyuk@gmail.com>
2018-08-27 09:59:35 +0300

    Change style

--- backend/Watcher.Core/MappingProfiles/NotificationSettingsProfile.cs
+++ backend/Watcher.Core/MappingProfiles/NotificationSettingsProfile.cs
@@ -11,12 +11,19 @@
         public NotificationSettingsProfile()
         {
             CreateMap<NotificationSetting, NotificationSetting>()
-               .ForMember(d => d.Id, o => o.Ignore()); // Don't Map Id because It is useless for Ids when updating
-
+               .ForMember(d => d.Id, o => o.Ignore()) // Don't Map Id because It is useless for Ids when updating
+                .ForMember(d => d.UserId, o => o.Ignore())
+                .ForMember(d => d.User, o => o.Ignore())
+                .ForMember(d => d.Type, o => o.Ignore())
+                .ForMember(d => d.Notifications, o => o.Ignore());
+            
             CreateMap<NotificationSettingDto, NotificationSetting>();
 
             CreateMap<NotificationSettingRequest, NotificationSetting>()
                 .ForMember(d => d.Id, o => o.Ignore());
+
+            CreateMap<NotificationSettingUpdateRequest, NotificationSetting>();
+
         }
     }
 }
