b50affd3aa448564b264ea4116d75f61d85dcb2f
Volodymyr <volodymyr_bilyk@outlook.com>
2018-08-28 16:03:41 +0300

    Moved notification block to own component

--- frontend/src/app/shell/header/header.component.ts
+++ frontend/src/app/shell/header/header.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem, Message } from 'primeng/api';
-import { SampleRequest } from '../../shared/models/sample-request.model';
-import { SampleEnum } from '../../shared/models/sample-enum.enum';
-import { SampleDto } from '../../shared/models/sample-dto.model';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
+
+import { PathService } from '../../core/services/path.service';
 import { MessageService } from 'primeng/api';
 import { AuthService } from '../../core/services/auth.service';
-import { Organization } from '../../shared/models/organization.model';
 import { UserService } from '../../core/services/user.service';
-import { User } from '../../shared/models/user.model';
 import { ToastrService } from '../../core/services/toastr.service';
-import { Router, RouterEvent, ActivatedRoute } from '@angular/router';
-import { NotificationService } from '../../core/services/notification.service';
-import { Notification } from '../../shared/models/notification.model';
-import { NotificationType } from '../../shared/models/notification-type.enum';
-import { NotificationsHubService } from '../../core/hubs/notifications.hub';
-import { PathService } from '../../core/services/path.service';
+
+import { Organization } from '../../shared/models/organization.model';
+import { User } from '../../shared/models/user.model';
+
 
 @Component({
   selector: 'app-header',
@@ -23,12 +19,10 @@ import { PathService } from '../../core/services/path.service';
   providers: [MessageService]
 })
 export class HeaderComponent implements OnInit {
-  canCreateSample = false;
-  msgs: Message[] = [];
-  samples: SampleDto[] = [];
-  notificationsNumber = 0;
-  messagesNumber = 2;
-  isNotificationShow: boolean;
+
+  onDisplayNotificationBlock = new EventEmitter<boolean>();
+  isNotificationBlockShow: boolean;
+  unreadedNotification = 0;
 
   currentUser: User;
   currentOrganizationName: string;
@@ -37,49 +31,39 @@ export class HeaderComponent implements OnInit {
   adminItems: MenuItem[];
   cogItems: MenuItem[];
   orgItems: MenuItem[];
-  notifications: Notification[];
   displayAddNewOrganization = false;
 
-  constructor(private notificationsHubService: NotificationsHubService,
-    private messageService: MessageService,
+  constructor(
     private userService: UserService,
     private toastrService: ToastrService,
     private router: Router,
     private authService: AuthService,
-    private notificationsService: NotificationService,
-    private pathService: PathService) {
-    this.conectToNotificationsHub();
-    this.subscribeToNotificationsEvents();
-  }
+    private pathService: PathService) { }
 
   onFeedback(): void {
     this.router.navigate(['/user/feedback']);
   }
 
-  private conectToNotificationsHub(): void {
-    this.notificationsHubService.connectToSignalR();
-  }
-
-  private subscribeToNotificationsEvents(): void {
-    this.notificationsHubService.notificationReceived.subscribe((value: Notification) => {
-      value.type = NotificationType[value.notificationSetting.type].toLowerCase();
-
-      if (NotificationType[value.notificationSetting.type] !== 'Chat' && !value.notificationSetting.isMute) {
-        this.notificationsNumber++;
-      }
-      this.notifications.unshift(value);
-    });
-  }
-
   bellClick(): void {
-    this.isNotificationShow = !this.isNotificationShow;
-    if (this.isNotificationShow) {
+    this.isNotificationBlockShow = !this.isNotificationBlockShow;
+    this.onDisplayNotificationBlock.emit(this.isNotificationBlockShow);
+
+    if (this.isNotificationBlockShow) {
       this.forceActiveButtonState('bell-button');
     } else {
       this.removeActiveButttonState('bell-button');
       return;
     }
-    this.notificationsToItems();
+  }
+
+  changeCounter(number) {
+    this.unreadedNotification = number;
+  }
+
+  closeNotificationBlock() {
+    this.isNotificationBlockShow = false;
+    this.onDisplayNotificationBlock.emit(this.isNotificationBlockShow);
+    this.removeActiveButttonState('bell-button');
   }
 
   forceActiveButtonState(id: string): void {
@@ -90,66 +74,6 @@ export class HeaderComponent implements OnInit {
   removeActiveButttonState(id: string): void {
     const btn = document.getElementById(id);
     btn.style.backgroundColor = '#313232';
-  }
-
-  notificationsToItems(): void {
-    this.notifications = [];
-    this.notificationsService.getAll(this.authService.getCurrentUser().id).subscribe((value: Notification[]) => {
-      this.notificationsNumber = this.calcNotReadNotification(value);
-
-      value.forEach(item => {
-        item.type = NotificationType[item.notificationSetting.type].toLowerCase();
-        if (item.type !== 'chat') {
-          this.notifications.unshift(item);
-        }
-      });
-    });
-  }
-
-  calcNotReadNotification(allNotifications: Notification[]): number {
-    return allNotifications.filter(item => item.wasRead === false &&
-      item.notificationSetting.isMute === false &&
-      NotificationType[item.notificationSetting.type] !== 'Chat').length;
-  }
-
-  markAsRead(): void {
-    let notReadNotifications: Notification[];
-
-    notReadNotifications = this.notifications.filter(item => item.wasRead === false);
-    notReadNotifications.forEach(item => {
-      item.wasRead = true;
-    });
-
-    this.notificationsService.updateAll(notReadNotifications).subscribe(value => this.notificationsNumber = 0);
-  }
-
-  close(): void {
-    this.isNotificationShow = false;
-    this.removeActiveButttonState('bell-button');
-  }
-
-  // TODO: methods for SignalR Tests
-  createSample(): void {
-    const req: SampleRequest = {
-      name: 'Test',
-      count: 12,
-      dateOfCreation: new Date(2017, 1, 1),
-      sampleField: SampleEnum.FirstItem
-    };
-
-    this.notificationsHubService.createSample(req);
-  }
-
-  echoToServer() {
-    this.notificationsHubService.echo();
-  }
-
-  sendMess() {
-    this.notificationsHubService.send('in2Kvey8O0dK1LACXH6HYMaPG102', 'From Sdadadafsd message');
-  }
-
-  connectToServer() {
-    this.notificationsHubService.connectToSignalR();
   }
 
   logout(): void {
@@ -201,7 +125,6 @@ export class HeaderComponent implements OnInit {
         this.fillOrganizations();
       }
     );
-    this.notificationsToItems();
   }
 
   private fillOrganizations(): void {
