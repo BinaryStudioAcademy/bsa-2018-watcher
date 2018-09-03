ffcc2fbbb39a3156056cce42921a11b247f97f48
Volodymyr <volodymyr_bilyk@outlook.com>
2018-09-03 11:15:44 +0300

    Fix

--- frontend/src/app/shell/left-side-menu/left-side-menu.component.ts
+++ frontend/src/app/shell/left-side-menu/left-side-menu.component.ts
@@ -158,7 +158,7 @@ export class LeftSideMenuComponent implements OnInit, AfterContentChecked, After
 
   async deleteInstance(id: number, index: number) {
     if (await this.toastrService.confirm('You sure you want to delete this instance? ')) {
-      this.popupMessage = `Instance is deleting`
+      this.popupMessage = `Instance is deleting`;
       this.isDeleting = true;
       this.instanceService.delete(id).subscribe((res: Response) => {
         this.instanceService.instanceRemoved.emit(id);
