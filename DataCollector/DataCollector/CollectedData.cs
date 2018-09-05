f3708f30362cba985b6738e74b7174ad1bef6783
Pavel Boliubash <5avel@hotmail.com>
2018-09-05 17:00:42 +0300

    Updated Collected data model

--- DataCollector/DataCollector/CollectedData.cs
+++ DataCollector/DataCollector/CollectedData.cs
@@ -21,13 +21,13 @@ namespace DataCollector
         public int ProcessesCount { get; set; }
         public List<ProcessData> Processes { get; set; }
 
-        public float FreeRamMBytes { get; set; }
+        public float UsageRamMBytes { get; set; }
         public float TotalRamMBytes { get; set; }
         public float RamUsagePercentage { get; set; }
 
         public float InterruptsPerSeconds { get; set; }
 
-        public float LocalDiskFreeMBytes { get; set; }
+        public float LocalDiskUsageMBytes { get; set; }
         public float LocalDiskTotalMBytes { get; set; }
         public float LocalDiskUsagePercentage { get; set; }
 
