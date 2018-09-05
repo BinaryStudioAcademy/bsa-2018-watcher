c905b54baaa62760f176c9d35a155c8a0b6259b1
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-09-05 13:54:07 +0300

    Fix CollectedData entity

--- DataAccumulator/DataAccumulator.DataAccessLayer/Entities/CollectedData.cs
+++ DataAccumulator/DataAccumulator.DataAccessLayer/Entities/CollectedData.cs
@@ -25,10 +25,7 @@ namespace DataAccumulator.DataAccessLayer.Entities
         public CollectedDataType CollectedDataType { get; set; }
 
         public int ProcessesCount { get; set; }
-
-        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfDocuments)]
         public List<ProcessData> Processes { get; set; }
-
         public float FreeRamMBytes { get; set; }
         public float TotalRamMBytes { get; set; }
         public float FreeRamPercentage { get; set; }
