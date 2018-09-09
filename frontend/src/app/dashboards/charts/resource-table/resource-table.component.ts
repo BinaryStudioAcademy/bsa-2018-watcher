a5fd9a427fe3812fe7cc189bcde1d4cbbbca5de6
Volodymyr <volodymyr_bilyk@outlook.com>
2018-09-09 16:54:32 +0300

    Fixed table

--- frontend/src/app/dashboards/charts/resource-table/resource-table.component.ts
+++ frontend/src/app/dashboards/charts/resource-table/resource-table.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { DataProperty, dataPropertyLables } from '../../../shared/models/data-property.enum';
 import { CollectedData } from '../../../shared/models/collected-data.model';
+import { ProcessData } from '../../../shared/models/process-data.model';
 
 @Component({
   selector: 'app-resource-table',
@@ -29,14 +30,26 @@ export class ResourceTableComponent {
       case DataProperty.pRam:
         return `RAM (${Math.round(this.data.ramUsagePercentage) || '...'}%)`;
       case DataProperty.ramMBytes:
-        return `RAM (${this.data.usageRamMBytes || '...'}MB)`;
+        return `RAM (${Math.round(this.data.usageRamMBytes) || '...'}MB)`;
       default:
         return this.labels[value];
     }
   }
 
-  getColor(percent: number) {
-    return 'rgba(' + [255, 255 - percent * 4, 255 - percent * 18, 0.6].join(',') + ')';
+  getColor(process: ProcessData, col: DataProperty) {
+    switch (col) {
+      case DataProperty.ramMBytes:
+        const percent = (process[DataProperty[col]] / this.data.totalRamMBytes) * 100;
+        return this.calculateColor(percent);
+      case DataProperty.pCpu:
+      case DataProperty.pRam:
+      const value = process[DataProperty[col]];
+        return this.calculateColor(value);
+    }
+  }
+
+  calculateColor(percent: number) {
+    return 'rgba(' + [255, 255 - percent * 3, 255 - percent * 18, 0.6].join(',') + ')';
   }
 
   getValue(value) {
@@ -45,5 +58,23 @@ export class ResourceTableComponent {
       return value;
     }
     return Math.round(value * 100) / 100;
+  }
+
+  getValueFor(process: ProcessData, col: DataProperty) {
+    const value = process[DataProperty[col]];
+    const result = isNaN(value);
+    if (result) {
+      return value;
+    }
+
+    switch (col) {
+      case DataProperty.pCpu:
+      case DataProperty.pRam:
+        return `${Math.round(value * 100) / 100}%`;
+      case DataProperty.ramMBytes:
+        return `${Math.round(value * 100) / 100} MB`;
+      default:
+        return Math.round(value * 100) / 100;
+    }
   }
 }
