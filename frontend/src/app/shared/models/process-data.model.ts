1822c0e798ebbd5c86f441644ec43794396a96f9
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2018-09-09 15:07:58 +0300

    proccess data in frontend

--- frontend/src/app/shared/models/process-data.model.ts
+++ frontend/src/app/shared/models/process-data.model.ts
@@ -1,6 +1,15 @@
 export interface ProcessData {
     name: string;
+
     ramMBytes: number;
+    ramMBytesMax?: number;
+    ramMBytesMin?: number;
+
     pRam: number;
+    pRamMax?: number;
+    pRamMin?: number;
+
     pCpu: number;
+    pCpuMax?: number;
+    pCpuMin?: number;
 }
