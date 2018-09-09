3d9332da5a0007857f08c97377e81c15fb2dfc66
Pavel Boliubash <5avel@hotmail.com>
2018-09-09 17:12:15 +0300

    Added pTable and CollectorApp type

--- frontend/src/app/admin/data-collector/data-collector.component.ts
+++ frontend/src/app/admin/data-collector/data-collector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CollectorApp } from '../../shared/models/collector-app.model';
 
 @Component({
   selector: 'app-data-collector',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataCollectorComponent implements OnInit {
 
+  collectorApps: CollectorApp[] = [{
+    id: 1,
+    createdAt: new Date(),
+    version: '1.0.3',
+    exeLink: '',
+    debLink: '',
+    tgzLink: '',
+    isActual: true
+  }, {
+    id: 2,
+    createdAt: new Date(),
+    version: '1.0.4',
+    exeLink: '',
+    debLink: '',
+    tgzLink: '',
+    isActual: false
+  }];
+  collectorApp: CollectorApp;
+  currentCollectorApp: CollectorApp;
+  displayPopup: boolean;
+  totalRecords: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.totalRecords = 2;
   }
 
 }
