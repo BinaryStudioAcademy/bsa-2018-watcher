327e296ace15f07cbf94905b2a7b74ecb2fbfc3f
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2018-09-10 15:51:58 +0300

    added instanceSettings message

---
+++ backend/ServiceBus.Shared/Messages/InstanceSettingsMessage.cs
@@ -0,0 +1,30 @@
+﻿namespace ServiceBus.Shared.Messages
+{
+    using System;
+
+    class InstanceSettingsMessage : InstanceMessage
+    {
+        public InstanceSettingsMessage(Guid instanceId, 
+                                       bool hourAggregation,
+                                       bool dayAggregation,
+                                       bool monthAggregation,
+                                       float cpuMax,
+                                       float ramMax,
+                                       float diskMax): base(instanceId)
+        {
+            AggregationForHour = hourAggregation;
+            AggregationForDay = dayAggregation;
+            AggregationForMonth = monthAggregation;
+            CpuMaxPercent = cpuMax;
+            RamMaxPercent = ramMax;
+            DiskMaxPercent = diskMax;
+        }
+        public bool AggregationForHour { get; set; }
+        public bool AggregationForDay { get; set; }
+        public bool AggregationForMonth { get; set; }
+
+        public float CpuMaxPercent { get; set; }
+        public float RamMaxPercent { get; set; }
+        public float DiskMaxPercent { get; set; }
+    }
+}
