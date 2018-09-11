1059fe5b196428e965fb86f0593bc80c5491e309
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2018-09-11 20:19:39 +0300

    text notifications

--- DataAccumulator/DataAccumulator.BusinessLayer/Validators/ThresholdsValidatorCore.cs
+++ DataAccumulator/DataAccumulator.BusinessLayer/Validators/ThresholdsValidatorCore.cs
@@ -11,6 +11,8 @@ using DataAccumulator.Shared.Models;
 
 using ServiceBus.Shared.Messages;
 using ServiceBus.Shared.Enums;
+using System.Text;
+using System.Linq;
 
 namespace DataAccumulator.BusinessLayer.Validators
 {
@@ -33,24 +35,49 @@ namespace DataAccumulator.BusinessLayer.Validators
                 var instanceValidatorDto = await _instanceValidatorService
                     .GetEntityByInstanceIdAsync(collectedDataDto.ClientId);
 
+
                 var validator = new CollectedDataThresholdsValidator(instanceValidatorDto);
 
                 var validatorParams = new List<string>();
-                if (instanceValidatorDto.RamValidator) validatorParams.Add("RamUsage");
-                if (instanceValidatorDto.LocalDiskVallidator) validatorParams.Add("LocalDiskUsage");
-                if (instanceValidatorDto.CpuValidator) validatorParams.Add("CpuUsage");
+                if (instanceValidatorDto.RamValidator) validatorParams.Add("Ram");
+                if (instanceValidatorDto.LocalDiskVallidator) validatorParams.Add("Disk");
+                if (instanceValidatorDto.CpuValidator) validatorParams.Add("Cpu");
 
                 var context = new ValidationContext<CollectedDataDto>(collectedDataDto, 
                     new PropertyChain(), new RulesetValidatorSelector(validatorParams.ToArray()));
 
                 var validationResult = await validator.ValidateAsync(context);
-               var validatorErrors = validationResult.Errors;
+
                 if (!validationResult.IsValid)
                 {
+                    StringBuilder textMessage = new StringBuilder();
+
+                    textMessage.Append(instanceValidatorDto.ClientId);
+                    foreach (var item in validationResult.Errors)
+                    {
+                        var name = item.FormattedMessagePlaceholderValues["PropertyName"]
+                            .ToString();
+
+                        int index = name.IndexOf("Percentage");
+                        name = (index < 0)
+                            ? name
+                            : name.Remove(index, "Percentage".Length);
+
+                        index = name.IndexOf("Local");
+                        name = (index < 0)
+                           ? name
+                           : name.Remove(index, "Local".Length);
+
+                        name = name.ToLower().PadRight(12, ' ');
+
+                        textMessage.Append(" " + name + " has reached " +item.AttemptedValue.ToString().Substring(0, 4) + "% ");
+                    }
                     var message = new InstanceNotificationMessage()
                     {
                         InstanceId = instanceValidatorDto.ClientId,
-                        Text = validationResult.ToString()
+                        CreatedAt = DateTime.Now,
+                        Type = InstanceNotifyType.Error,
+                        Text = textMessage.ToString()
                     };
                     await _serviceBusProvider.SendNotificationMessage(message);
                 }
--- DataAccumulator/DataAccumulator.Shared/Models/InstanceSettingsDto.cs
+++ DataAccumulator/DataAccumulator.Shared/Models/InstanceSettingsDto.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using DataAccumulator.Shared.Interfaces;
 
 namespace DataAccumulator.Shared.Models
@@ -9,18 +7,19 @@ namespace DataAccumulator.Shared.Models
     {
         // Id - InstanceValidator identification number
         public Guid Id { get; set; }
+
         // ClientId - Instance identification number
         public Guid ClientId { get; set; }
 
         public bool IsActive { get; set; }
 
-        public bool RamValidator { get; set; } = true;
+        public bool RamValidator { get; set; }
         public float RamUsagePercentageMax { get; set; }
 
-        public bool LocalDiskVallidator { get; set; } = true;
+        public bool LocalDiskVallidator { get; set; }
         public float LocalDiskUsagePercentageMax { get; set; }
 
-        public bool CpuValidator { get; set; } = true;
+        public bool CpuValidator { get; set; }
         public float CpuUsagePercentageMax { get; set; }
     }
 }
