6a4a02110dd8ee653fb2fa44bf6064a0cb42f7ff
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2018-09-12 22:27:01 +0300

    notifications front

--- frontend/src/app/notification/notification-block/notification-block.component.ts
+++ frontend/src/app/notification/notification-block/notification-block.component.ts
@@ -7,6 +7,7 @@ import { SystemToastrService } from '../../core/services/system-toastr.service';
 
 import { NotificationType } from '../../shared/models/notification-type.enum';
 import { Notification } from '../../shared/models/notification.model';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -31,7 +32,8 @@ export class NotificationBlockComponent implements OnInit {
     private notificationsHubService: NotificationsHubService,
     private authService: AuthService,
     private notificationsService: NotificationService,
-    private systemToastrService: SystemToastrService) { }
+    private systemToastrService: SystemToastrService,
+    private router: Router) { }
 
   ngOnInit() {
     this.loadNotifications();
@@ -104,5 +106,10 @@ export class NotificationBlockComponent implements OnInit {
     const index = this.notifications.findIndex(item => item.id === id);
     this.notifications.splice(index, 1);
     this.notificationsHubService.delete(notify);
+  }
+
+  redirectToInstance(notification): void {
+    const url = [`/user/instances/${notification.instanceId}/${notification.instanceGuidId}/dashboards`];
+    this.router.navigate(url);
   }
 }
