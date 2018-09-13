aaad1c838c2633081b4c5700feed2b6a94a8d3d2
Pavel Boliubash <5avel@hotmail.com>
2018-09-13 09:04:14 +0300

    add migration Added_CollectorAppVersions_Entity

---
+++ backend/Watcher.DataAccess/Repositories/CollectorAppVersionRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Watcher.DataAccess.Repositories
+{
+    using AutoMapper;
+    using Watcher.DataAccess.Data;
+    using Watcher.DataAccess.Entities;
+    using Watcher.DataAccess.Interfaces.Repositories;
+    class CollectorAppVersionRepository : Repository<CollectorAppVersion, int>, ICollectorAppVersionRepository
+    {
+        public CollectorAppVersionRepository(WatcherDbContext context, IMapper mapper)
+           : base(context, mapper)
+        {
+
+        }
+    }
+}
